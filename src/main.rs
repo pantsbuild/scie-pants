14dc9f88f8c89075047cc6e0d1371758d3111c0f
John Sirois <john.sirois@gmail.com>
2022-11-29 15:22:45 -0800

    Start to factor out a module for the logic.

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,103 @@
-use std::path::{Path, PathBuf};
-
 use log::info;
-use logging_timer::{time, timer, Level};
-use serde::Deserialize;
+use logging_timer::{timer, Level};
 
-#[derive(Deserialize)]
-struct Global {
-    pants_version: String,
-}
+use crate::config::PantsConfig;
 
-fn default_setup_cache() -> Result<PathBuf, String> {
-    if let Some(cache_dir) = dirs::cache_dir() {
-        Ok(cache_dir.join("pants").join("setup"))
-    } else if let Some(home_dir) = dirs::home_dir() {
-        Ok(home_dir.join(".pants-setup"))
-    } else {
-        Err(
-            "Failed to determine a reasonable default cache directory for Pants setup and failed \
-            to find a fall back user home directory to establish ~/.pants-setup"
-                .to_string(),
-        )
+mod config {
+    use std::path::{Path, PathBuf};
+
+    use logging_timer::time;
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    pub(crate) struct Global {
+        pub pants_version: String,
     }
-}
 
-#[derive(Default, Deserialize)]
-struct Setup {
-    cache: Option<PathBuf>,
-}
+    #[derive(Default, Deserialize)]
+    pub(crate) struct Setup {
+        cache: Option<PathBuf>,
+    }
 
-#[derive(Deserialize)]
-struct Config {
-    #[serde(rename = "GLOBAL")]
-    global: Global,
-    #[serde(default)]
-    setup: Setup,
-}
-
-struct PantsConfig {
-    build_root: PathBuf,
-    config: Config,
-}
-
-impl PantsConfig {
-    #[time("debug")]
-    fn find(start_dir: Option<PathBuf>) -> Result<PantsConfig, String> {
-        let start_search = if let Some(cwd) = start_dir {
-            cwd
-        } else {
-            std::env::current_dir().map_err(|e| format!("{e}"))?
-        };
-
-        let mut cwd = start_search.as_path();
-        loop {
-            let config_path = cwd.join("pants.toml");
-            if config_path.is_file() {
-                return Self::parse(cwd, config_path.as_path());
+    impl Setup {
+        pub(crate) fn cache(&mut self) -> Result<PathBuf, String> {
+            if let Some(setup_cache) = self.cache.as_ref() {
+                return Ok(setup_cache.clone());
             }
-            cwd = cwd.parent().ok_or_else(|| {
+
+            let default_cache = if let Some(cache_dir) = dirs::cache_dir() {
+                cache_dir.join("pants").join("setup")
+            } else if let Some(home_dir) = dirs::home_dir() {
+                home_dir.join(".pants-setup")
+            } else {
+                return Err(
+                    "Failed to determine a reasonable default cache directory for Pants setup and \
+                failed to find a fall back user home directory to establish \
+                ~/.pants-setup"
+                        .to_string(),
+                );
+            };
+            self.cache = Some(default_cache.clone());
+            Ok(default_cache)
+        }
+    }
+
+    #[derive(Deserialize)]
+    pub(crate) struct Config {
+        #[serde(rename = "GLOBAL")]
+        pub(crate) global: Global,
+        #[serde(default)]
+        pub(crate) setup: Setup,
+    }
+
+    pub(crate) struct PantsConfig {
+        pub(crate) build_root: PathBuf,
+        pub(crate) config: Config,
+    }
+
+    impl PantsConfig {
+        #[time("debug")]
+        pub(crate) fn find(start_dir: Option<PathBuf>) -> Result<PantsConfig, String> {
+            let start_search = if let Some(cwd) = start_dir {
+                cwd
+            } else {
+                std::env::current_dir().map_err(|e| format!("{e}"))?
+            };
+
+            let mut cwd = start_search.as_path();
+            loop {
+                let config_path = cwd.join("pants.toml");
+                if config_path.is_file() {
+                    return Self::parse(cwd, config_path.as_path());
+                }
+                cwd = cwd.parent().ok_or_else(|| {
+                    format!(
+                        "Failed to find pants.toml starting at {start_search}",
+                        start_search = start_search.display()
+                    )
+                })?;
+            }
+        }
+
+        #[time("debug")]
+        fn parse(build_root: &Path, config: &Path) -> Result<PantsConfig, String> {
+            let contents = std::fs::read(config).map_err(|e| {
                 format!(
-                    "Failed to find pants.toml starting at {start_search}",
-                    start_search = start_search.display()
+                    "Failed to read Pants config from {path}: {e}",
+                    path = config.display()
                 )
             })?;
+            let config: Config = toml::from_slice(contents.as_slice()).map_err(|e| {
+                format!(
+                    "Failed to parse Pants config from {path}: {e}",
+                    path = config.display()
+                )
+            })?;
+            Ok(PantsConfig {
+                build_root: build_root.to_path_buf(),
+                config,
+            })
         }
-    }
-
-    #[time("debug")]
-    fn parse(build_root: &Path, config: &Path) -> Result<PantsConfig, String> {
-        let contents = std::fs::read(config).map_err(|e| {
-            format!(
-                "Failed to read Pants config from {path}: {e}",
-                path = config.display()
-            )
-        })?;
-        let config: Config = toml::from_slice(contents.as_slice()).map_err(|e| {
-            format!(
-                "Failed to parse Pants config from {path}: {e}",
-                path = config.display()
-            )
-        })?;
-        Ok(PantsConfig {
-            build_root: build_root.to_path_buf(),
-            config,
-        })
-    }
-
-    fn setup_cache(&mut self) -> Result<PathBuf, String> {
-        if let Some(setup_cache) = self.config.setup.cache.as_ref() {
-            return Ok(setup_cache.clone());
-        }
-
-        let default_setup_cache = default_setup_cache()?;
-        self.config.setup.cache = Some(default_setup_cache.clone());
-        Ok(default_setup_cache)
     }
 }
 
@@ -101,7 +106,7 @@ fn main() -> Result<(), String> {
     let _timer = timer!(Level::Debug; "MAIN");
 
     let mut pants_config = PantsConfig::find(None)?;
-    let setup_cache = pants_config.setup_cache()?;
+    let setup_cache = pants_config.config.setup.cache()?;
     info!(
         "Found Pants build root at {build_root} and setup cache at {setup_cache}",
         build_root = pants_config.build_root.display(),
