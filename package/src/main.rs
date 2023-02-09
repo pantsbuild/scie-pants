4522969bdf3a416324f0d0254c0e7919ebb94c86
John Sirois <john.sirois@gmail.com>
2023-02-08 16:15:04 -0800

    Debug Pants 2.15.0rc4 hangs.

--- package/src/main.rs
+++ package/src/main.rs
@@ -849,10 +849,7 @@ fn test(
                 false,
                 r#"
                 [GLOBAL]
-                pants_version = "2.14.1"
-                # TODO(John Sirois): This works around ongoing issues with pantsd termination
-                # variously crashing or hanging depending on Pants version.
-                pantsd = false
+                pants_version = "2.15.0rc4"
                 [anonymous-telemetry]
                 enabled = false
                 "#,
